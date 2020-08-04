2d3638418aa6ca0353c1691e9f9464d58355b3b7
SlopingAcorn38 <tkrivobokova@gmail.com>
2020-08-04 22:59:10 +0300

    create: added animation

--- js/main.js
+++ js/main.js
@@ -67,5 +67,6 @@ $(document).ready(function () {
       },
     });
   });
-  $(".mask").mask("+7 (000) 000-00-00", { placeholder: "+7 (999) 999-99-99" });
+  $(".mask").mask("+7 (000) 000-00-00");
+  AOS.init();
 });
