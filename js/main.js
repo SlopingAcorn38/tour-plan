3c085ef071f3baa18c0964fcd7376c98b650c8d1
SlopingAcorn38 <tkrivobokova@gmail.com>
2020-08-04 20:45:35 +0300

    create: added validation to forms

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     },
     effect: "coverflow",
   });
-  
+
   var reviewsSlider = new Swiper(".reviews-slider", {
     loop: true,
     navigation: {
@@ -16,36 +16,55 @@ $(document).ready(function () {
     },
   });
   $(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpeg" });
-  
+
   var menuButton = $(".menu-button");
   menuButton.on("click", function () {
     $(".navbar-bottom").toggleClass("navbar-bottom--visible");
   });
-  
+
   var modalButton = $('[data-toggle="modal"]');
-  var closeModalButton = $('.modal__close');
-  modalButton.on('click', openModal);
-  closeModalButton.on('click', closeModal);
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
 
   function openModal() {
-    var targetModal = $(this).attr('data-href');
-    $(targetModal).find('.modal__overlay').addClass('modal__overlay--visible');
-    $(targetModal).find('.modal__dialog').addClass('modal__dialog--visible');
+    var targetModal = $(this).attr("data-href");
+    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
+    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
   }
   function closeModal(event) {
     event.preventDefault();
-    var modalOverlay = $('.modal__overlay');
-    var modalDialog = $('.modal__dialog');
-    modalOverlay.removeClass('modal__overlay--visible');
-    modalDialog.removeClass('modal__dialog--visible');
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
   }
-  $(document).keydown(function(event) { 
-    if (event.keyCode == 27) { 
+  $(document).keydown(function (event) {
+    if (event.keyCode == 27) {
       event.preventDefault();
-      var modalOverlay = $('.modal__overlay');
-      var modalDialog = $('.modal__dialog');
-      modalOverlay.removeClass('modal__overlay--visible');
-      modalDialog.removeClass('modal__dialog--visible');
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass("modal__overlay--visible");
+      modalDialog.removeClass("modal__dialog--visible");
     }
+  });
+
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name should be at least 2 symbols long",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Please type the phone",
+        },
+      },
+    });
   });
 });
