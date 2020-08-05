23f3a22a8def0e39d5477d15cb5a906aaa022820
SlopingAcorn38 <tkrivobokova@gmail.com>
2020-08-05 12:47:07 +0300

    create: added subscribtion mailer

---
+++ subscribe.php
@@ -0,0 +1,54 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+
+// Формирование самого письма
+$title = "New request from Best Tour Plan";
+$body = "
+    <h2>Thank you for subscribing</h2>
+    $email successfully subscribed for Best Tour Plan newsletter
+    ";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'tatjanatestmail@gmail.com'; // Логин на почте
+    $mail->Password   = '_vRtkG8QUX'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('tatjanatestmail@gmail.com', 'Tatjana TestMail'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress($email);  
+
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
