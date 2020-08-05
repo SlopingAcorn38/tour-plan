351f8fba2aefe10de25ea7a78c7e777ba2cbe553
SlopingAcorn38 <tkrivobokova@gmail.com>
2020-08-05 21:53:52 +0300

    fix: minified js

--- js/main.js
+++ js/main.js
@@ -1,72 +1,63 @@
 $(document).ready(function () {
-  var hotelSlider = new Swiper(".hotel-slider", {
-    loop: true,
+  new Swiper(".hotel-slider", {
+    loop: !0,
     navigation: {
       nextEl: ".hotel-slider__button--next",
       prevEl: ".hotel-slider__button--prev",
     },
     effect: "coverflow",
-  });
-
-  var reviewsSlider = new Swiper(".reviews-slider", {
-    loop: true,
-    navigation: {
-      nextEl: ".reviews-slider__button--next",
-      prevEl: ".reviews-slider__button--prev",
-    },
-  });
-  $(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpeg" });
-
-  var menuButton = $(".menu-button");
-  menuButton.on("click", function () {
-    $(".navbar-bottom").toggleClass("navbar-bottom--visible");
-  });
-
-  var modalButton = $('[data-toggle="modal"]');
-  var closeModalButton = $(".modal__close");
-  modalButton.on("click", openModal);
-  closeModalButton.on("click", closeModal);
-
-  function openModal() {
-    var targetModal = $(this).attr("data-href");
-    $(targetModal).find(".modal__overlay").addClass("modal__overlay--visible");
-    $(targetModal).find(".modal__dialog").addClass("modal__dialog--visible");
-  }
-  function closeModal(event) {
-    event.preventDefault();
-    var modalOverlay = $(".modal__overlay");
-    var modalDialog = $(".modal__dialog");
-    modalOverlay.removeClass("modal__overlay--visible");
-    modalDialog.removeClass("modal__dialog--visible");
-  }
-  $(document).keydown(function (event) {
-    if (event.keyCode == 27) {
-      event.preventDefault();
-      var modalOverlay = $(".modal__overlay");
-      var modalDialog = $(".modal__dialog");
-      modalOverlay.removeClass("modal__overlay--visible");
-      modalDialog.removeClass("modal__dialog--visible");
-    }
-  });
-
-  $(".form").each(function () {
-    $(this).validate({
-      errorClass: "invalid",
-      messages: {
-        name: {
-          required: "Please specify your name",
-          minlength: "Name should be at least 2 symbols long",
-        },
-        email: {
-          required: "We need your email address to contact you",
-          email: "Your email address must be in the format of name@domain.com",
-        },
-        phone: {
-          required: "Please type the phone",
-        },
+  }),
+    new Swiper(".reviews-slider", {
+      loop: !0,
+      navigation: {
+        nextEl: ".reviews-slider__button--next",
+        prevEl: ".reviews-slider__button--prev",
       },
     });
-  });
-  $(".mask").mask("+7 (000) 000-00-00");
-  AOS.init();
+  $(".parallax-window").parallax({ imageSrc: "img/newsletter-bg.jpeg" }),
+    $(".menu-button").on("click", function () {
+      $(".navbar-bottom").toggleClass("navbar-bottom--visible");
+    });
+  var e = $('[data-toggle="modal"]'),
+    a = $(".modal__close");
+  e.on("click", function () {
+    var e = $(this).attr("data-href");
+    $(e).find(".modal__overlay").addClass("modal__overlay--visible"),
+      $(e).find(".modal__dialog").addClass("modal__dialog--visible");
+  }),
+    a.on("click", function (e) {
+      e.preventDefault();
+      var a = $(".modal__overlay"),
+        o = $(".modal__dialog");
+      a.removeClass("modal__overlay--visible"),
+        o.removeClass("modal__dialog--visible");
+    }),
+    $(document).keydown(function (e) {
+      if (27 == e.keyCode) {
+        e.preventDefault();
+        var a = $(".modal__overlay"),
+          o = $(".modal__dialog");
+        a.removeClass("modal__overlay--visible"),
+          o.removeClass("modal__dialog--visible");
+      }
+    }),
+    $(".form").each(function () {
+      $(this).validate({
+        errorClass: "invalid",
+        messages: {
+          name: {
+            required: "Please specify your name",
+            minlength: "Name should be at least 2 symbols long",
+          },
+          email: {
+            required: "We need your email address to contact you",
+            email:
+              "Your email address must be in the format of name@domain.com",
+          },
+          phone: { required: "Please type the phone" },
+        },
+      });
+    }),
+    $(".mask").mask("+7 (000) 000-00-00"),
+    AOS.init();
 });
