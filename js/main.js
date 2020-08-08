9b03b117e02e982f36b83fac2a4a10d11d7c3aa6
SlopingAcorn38 <tkrivobokova@gmail.com>
2020-08-08 10:42:43 +0300

    fix: changed map

--- js/main.js
+++ js/main.js
@@ -70,18 +70,9 @@ $(document).ready(function () {
   });
   $(".mask").mask("+7 (000) 000-00-00");
   AOS.init();
-});
-ymaps.ready(init);
-function init() {
-  // Создание карты.
-  var myMap = new ymaps.Map("map", {
-    // Координаты центра карты.
-    // Порядок по умолчанию: «широта, долгота».
-    // Чтобы не определять координаты центра карты вручную,
-    // воспользуйтесь инструментом Определение координат.
-    center: [59.438887, 24.746651],
-    // Уровень масштабирования. Допустимые значения:
-    // от 0 (весь мир) до 19.
-    zoom: 19,
+  const map = document.querySelector(".map");
+  map.addEventListener("click", () => {
+    map.innerHTML =
+      '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3952.0474214391784!2d98.2942563!3d7.8901079!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30503b7bfcd9f903%3A0xf7065fac1e3d7c48!2sDoubleTree%20by%20Hilton%20Phuket%20Banthai%20Resort!5e0!3m2!1sru!2sru!4v1596687171109!5m2!1sru!2sru" width="100%" height="100%" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
   });
-}
+});
